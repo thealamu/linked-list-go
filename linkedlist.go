85a1cd32e032e9208327e926b8147dc7d79a8dcc
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-01 09:15:03 +0100

    feat: add method to return linked lists size
    Includes a bugfix in the PushFront method
    where the first node was being added twice

--- linkedlist.go
+++ linkedlist.go
@@ -17,9 +17,22 @@ func New() *LinkedLists {
 func (l *LinkedLists) PushFront(item interface{}) {
 	if l.head == nil {
 		l.head = &node{data: item}
+		return
 	}
 
 	newHead := &node{data: item}
 	newHead.next = l.head
 	l.head = newHead
+}
+
+// Size returns the length of the linked lists
+func (l *LinkedLists) Size() int {
+	// walk the list to report the size
+	curr := l.head
+	i := 0
+	for curr != nil {
+		i++
+		curr = curr.next
+	}
+	return i
 }
--- linkedlist_test.go
+++ linkedlist_test.go
@@ -17,3 +17,21 @@ func TestPushFront(t *testing.T) {
 		t.Errorf("PushFront failed, expected '%+v' to be in front, got '%+v'", v3, l.head.data)
 	}
 }
+
+func TestSize(t *testing.T) {
+	l := New()
+
+	v1 := 1
+	v2 := "Hello"
+	v3 := "World"
+
+	l.PushFront(v1)
+	l.PushFront(v2)
+	l.PushFront(v3)
+
+	got := l.Size()
+
+	if got != 3 {
+		t.Errorf("expected size %d, got %d", 3, got)
+	}
+}
